3dd73cc572fe1aa3894b8e1ee9a43a32d0e52f07
teegz <eteaghan@gmail.com>
2023-05-13 18:29:25 +1200

    removed ignore

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ app.get("/post/:title",function(req,res) {
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log(`Server started on port ${port}`);
 });
