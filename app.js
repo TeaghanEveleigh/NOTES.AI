675e94b844c1b21c5b513707caa44a8d678e3daf
teegz <eteaghan@gmail.com>
2023-05-15 19:41:09 +1200

    working on delete feature

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ app.get("/post/:title", function(req, res) {
 });
 app.post("/", function(req, res) {
   console.log(req.body);
-  const id = mongoose.Types.ObjectId(req.body.id);
+  const id = new mongoose.Types.ObjectId(req.body.id);
   const action = req.body.action;
 
   console.log("the id of the note is " + id);
