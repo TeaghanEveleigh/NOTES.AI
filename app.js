ddd3a502723e501ff189f981e4cdecfa69e0f0da
teegz <eteaghan@gmail.com>
2023-05-15 17:27:36 +1200

    working on delete feature

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.post("/", function(req, res) {
   const action = req.body.action;
 
   console.log("the id is " + id);
-  const userId = req.user._id;
+  const userId = req.session.user._id;
   console.log("the id being passes is ")
   console.log(`Action: ${action}, ID: ${id}, User ID: ${userId}`);
 
